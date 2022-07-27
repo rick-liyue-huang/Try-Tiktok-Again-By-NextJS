cd2ccbfcac08dec23e14047251d0c7317bc550a5
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-07-27 12:10:55 +1000

    feat: complete layout in _app.tsx and navbar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SidebarComponent: React.FC = () => {
+  return <div>sidebar</div>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
+import { NavbarComponent } from '../components/Navbar';
+import { SidebarComponent } from '../components/Sidebar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // confirm the ssr or not from root
+  const [isSSR, setIsSSR] = useState<boolean>(true);
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  // is ssr will return null
+  if (isSSR) {
+    return null;
+  }
+
+  return (
+    <div>
+      {/* can also use layout */}
+      <NavbarComponent />
+      <div className={'flex gap-6 md:gap-20'}>
+        <div className={'h-[92vh] overflow-hidden xl:hover:overflow-auto'}>
+          <SidebarComponent />
+        </div>
+        <div
+          className={
+            'mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'
+          }
+        >
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
